d24edb851a8022e01458e57180956cf2e37004d2
ryood <kawanabe@gmail.com>
2017-07-24 22:19:09 +0900

    AD9833_FG: 周波数刻みを増やした

--- Arduino/AD9833_FG/AD9833_FG.ino
+++ Arduino/AD9833_FG/AD9833_FG.ino
@@ -56,28 +56,71 @@ const char waveFormName[][20] = {
   "TRI               "  
 };
 
-const uint32_t frequencyTable[] = {
+const uint32_t frequencyTable[] = {  
   1,
   2,
+  3,
+  4,
   5,
+  6,
+  7,
+  8,
+  9,
   10,
   20,
+  30,
+  40,
   50,
+  60,
+  70,
+  80,
+  90,
   100,
+  200,
+  300,
+  400,
   500,
+  600,
+  700,
+  800,
+  900,
   1000,
   2000,
+  3000,
+  4000,
   5000,
+  6000,
+  7000,
+  8000,
+  9000,
   10000,
   20000,
+  30000,
+  40000,
   50000,
+  60000,
+  70000,
+  80000,
+  90000,
   100000,
   200000,
+  300000,
+  400000,
   500000,
+  600000,
+  700000,
+  800000,
+  900000,
   1000000,
   2000000,
+  3000000,
+  4000000,
   5000000,
-  10000000  
+  6000000,
+  7000000,
+  8000000,
+  9000000,
+  10000000,
 };
 
 const int frequencyIndexMax = (sizeof(frequencyTable) / sizeof(uint32_t));
@@ -86,7 +129,7 @@ const int frequencyIndexMax = (sizeof(frequencyTable) / sizeof(uint32_t));
 // Variables
 //
 
-int frequencyIndex = 8; // 1kHz
+int frequencyIndex = 27; // 1kHz
 int waveFormIndex = 0;  // Sine wave
 
 int prevFrequencyIndex = frequencyIndex;
