e65e8b66fd914395dba14e60ae6f918e055c1f56
ryood <kawanabe@gmail.com>
2017-07-24 21:48:28 +0900

    AD9833_FG: Clean up

--- Arduino/AD9833_FG/AD9833_FG.ino
+++ Arduino/AD9833_FG/AD9833_FG.ino
@@ -1,3 +1,4 @@
+#include <SPI.h>
 #include <Wire.h>
 #include <stdio.h>
 
@@ -6,17 +7,37 @@
 #define TITLE_STR1  ("AD9833 FG")
 #define TITLE_STR2  ("20170724")
 
+//--------------------------------------------------------------------------------
 // pin assign
+//
+
+// AD8933
+const int FSYNC = 10;                       // Standard SPI pins for the AD9833 waveform generator.
+const int CLK = 13;                         // CLK and DATA pins are shared with the TFT display.
+const int DATA = 11;
+
+// Rotary Encoder
 const int RE_A = 2;
 const int RE_B = 3;
+
+// Tact SW
 const int SW1 = 4;
 
+// I2C LCD
 const int resetPin = 17;  // analog pin 3
 const int sdaPin = 18;    // analog pin 4
 const int sclPin = 19;    // analog pin 5
 const int i2cadr = 0x3e;
 const byte contrast = 32; // 最初は大きめにして調整する
 
+
+//--------------------------------------------------------------------------------
+// constants
+//
+
+const float refFreq = 25000000.0;           // On-board crystal reference frequency
+
+// Wave Form
 const int wfSine     = 0;
 const int wfSquare   = 1;
 const int wfTriangle = 2;
@@ -61,8 +82,12 @@ const uint32_t frequencyTable[] = {
 
 const int frequencyIndexMax = (sizeof(frequencyTable) / sizeof(uint32_t));
 
-int frequencyIndex = 0;
-int waveFormIndex = 0;
+//--------------------------------------------------------------------------------
+// Variables
+//
+
+int frequencyIndex = 8; // 1kHz
+int waveFormIndex = 0;  // Sine wave
 
 int prevFrequencyIndex = frequencyIndex;
 int prevWaveFormIndex = waveFormIndex;
@@ -91,6 +116,14 @@ void setup() {
   //lcd_move(0x40);
   lcd_pos(1, 1);
   lcd_puts(TITLE_STR2);
+
+  // AD8933
+  SPI.begin();
+  delay(50);
+  
+  AD9833reset();
+  delay(50);
+  AD9833setFrequency(frequencyTable[frequencyIndex], waveFormTable[wfSine]);
 }
 
 void loop()
@@ -192,7 +225,7 @@ void displayParamsI2CLCD(uint32_t frequency, int waveFormIndex)
 }
 
 //--------------------------------------------------------------------------------
-// akizuki AQM0802 / aitendo SPLC792-I2C
+// I2C LCD: akizuki AQM0802 / aitendo SPLC792-I2C
 //
 void lcd_cmd(byte x)
 {
@@ -245,6 +278,7 @@ void lcd_move(byte pos){
   lcd_cmd(0x80 | pos);
 }
 */
+
 void lcd_pos(byte raw, byte col) {
   lcd_cmd(0x80 | ((raw & 0x01) << 6) | col);
 }
@@ -253,14 +287,49 @@ void lcd_clear() {
   lcd_cmd(0x01);
 }
 
+//--------------------------------------------------------------------------------
+// AD9833: Waveform Generator
+//
 
-/*
-void AD9833setFrequency(uint32_t frequency, uint16_t Waveform) {
-  Serial.print("0x");
-  Serial.print(Waveform, HEX);
-  Serial.print('\t');
-  Serial.println(frequency);  
+// AD9833 documentation advises a 'Reset' on first applying power.
+void AD9833reset() {
+  WriteRegister(0x100);   // Write '1' to AD9833 Control register bit D8.
+  delay(10);
 }
-*/
+
+// Set the frequency and waveform registers in the AD9833.
+void AD9833setFrequency(long frequency, int Waveform) {
+
+  long FreqWord = (frequency * pow(2, 28)) / refFreq;
+
+  int MSB = (int)((FreqWord & 0xFFFC000) >> 14);    //Only lower 14 bits are used for data
+  int LSB = (int)(FreqWord & 0x3FFF);
+
+  //Set control bits 15 ande 14 to 0 and 1, respectively, for frequency register 0
+  LSB |= 0x4000;
+  MSB |= 0x4000;
+
+  WriteRegister(0x2100);
+  WriteRegister(LSB);                  // Write lower 16 bits to AD9833 registers
+  WriteRegister(MSB);                  // Write upper 16 bits to AD9833 registers.
+  WriteRegister(0xC000);               // Phase register
+  WriteRegister(Waveform);             // Exit & Reset to SINE, SQUARE or TRIANGLE
+}
+
+void WriteRegister(int dat) {
+
+  // Display and AD9833 use different SPI MODES so it has to be set for the AD9833 here.
+  SPI.setDataMode(SPI_MODE2);
+
+  digitalWrite(FSYNC, LOW);           // Set FSYNC low before writing to AD9833 registers
+  delayMicroseconds(10);              // Give AD9833 time to get ready to receive data.
+
+  SPI.transfer(highByte(dat));        // Each AD9833 register is 32 bits wide and each 16
+  SPI.transfer(lowByte(dat));         // bits has to be transferred as 2 x 8-bit bytes.
+
+  digitalWrite(FSYNC, HIGH);          //Write done. Set FSYNC high
+}
+
+
 
 
